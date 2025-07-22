9547184725eb05686aa50008482e35dbef6563f3
MeeReak <meereak168@gmail.com>
2025-07-22 15:09:01 +0700

    feat: add sitemap and robots.txt configuration for SEO

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://yourdomain.com",
+  i18n: {
+    locales: ["en", "km"],
+    defaultLocale: "en"
+  },
+  generateRobotsTxt: true
+};
