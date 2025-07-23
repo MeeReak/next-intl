5f01ba23ac1d5038409ae0a02357ac05872c4ea0
MeeReak <meereak168@gmail.com>
2025-07-23 14:24:31 +0700

    fix: add missing Open Graph image in generateMetadata for improved SEO

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -24,6 +24,7 @@ export async function generateMetadata({ params }) {
       description: t("meta.description"),
       type: "website",
       url: isDefaultLocale ? baseUrl : `${baseUrl}/${locale}`,
+      image: "https://tailwindcss.com/opengraph-image.jpg?fbee406903dc9e88",
       siteName: "Next Intl By Anonymous"
     },
     alternates: {
