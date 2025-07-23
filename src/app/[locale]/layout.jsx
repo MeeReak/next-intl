d858fc4d8849e762799eba2f2b67f8197c410fdf
MeeReak <meereak168@gmail.com>
2025-07-23 14:50:02 +0700

    fix: update app title in metadata and enhance favicon structure for improved consistency

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -17,7 +17,27 @@ export async function generateMetadata({ params }) {
     title: t("meta.title"),
     description: t("meta.description"),
     icons: {
-      icon: [{ url: "/favicon.ico", sizes: "any", type: "image/x-icon" }]
+      icon: [
+        { url: "/favicon.ico", sizes: "any", type: "image/x-icon" },
+        { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+        { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" }
+      ],
+      apple: [{ url: "/apple-touch-icon.png", sizes: "180x180" }],
+      shortcut: [{ url: "/favicon.ico" }],
+      other: [
+        {
+          rel: "android-chrome",
+          url: "/android-chrome-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          rel: "android-chrome",
+          url: "/android-chrome-512x512.png",
+          sizes: "512x512",
+          type: "image/png"
+        }
+      ]
     },
     openGraph: {
       title: t("meta.title"),
